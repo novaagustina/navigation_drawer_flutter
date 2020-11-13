f74ab1391351ed7db1fc442f84c52eeab4517a37
unknown <you@example.comnovaagustina777@gmail.com>
2020-11-13 10:09:11 +0700

    SideBar Navigation Menu Flutter by Nova Agustina

---
+++ lib/SecondFragment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text("Hello Fragment Travel"),
+    );
+  }
+}
